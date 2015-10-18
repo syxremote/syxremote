573d2ec7b1345695b8247bea88a39ed07b922954
Yuxin Shen <syxremote@gmail.com>
2015-10-18 22:53:56 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+get "static_pages/about"  # NEW LINE
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
